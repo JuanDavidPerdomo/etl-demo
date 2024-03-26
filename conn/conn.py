08e97384d0287bd03e04470ccb183a70e5c3ebab
Juan David Perdomo Quiroz <juanperdomo@JUAN>
2024-03-25 22:59:10 -0500

    added data transformation logic to complete etl process. Also it was added multiple extractions functions to build the master sales table

--- conn/conn.py
+++ conn/conn.py
@@ -9,43 +9,47 @@ load_dotenv(dotenv_path=env_path)
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-EXTERAL_USER = os.getenv('EXTERAL_USER')
-EXTERNAL_PASSWORD = os.getenv('EXTERNAL_PASSWORD')
-EXTERNAL_HOST = os.getenv('EXTERNAL_HOST')
-EXTERNAL_APP_NAME = os.getenv('EXTERNAL_APP_NAME')
+EXTERAL_USER = os.getenv("EXTERAL_USER")
+EXTERNAL_PASSWORD = os.getenv("EXTERNAL_PASSWORD")
+EXTERNAL_HOST = os.getenv("EXTERNAL_HOST")
+EXTERNAL_APP_NAME = os.getenv("EXTERNAL_APP_NAME")
 
 EXTERNAL_URI = f"mongodb+srv://{EXTERAL_USER}:{EXTERNAL_PASSWORD}@{EXTERNAL_HOST}/?retryWrites=true&w=majority&appName={EXTERNAL_APP_NAME}"
 
 # Create a new client and connect to the server
-EXTERNAL_CLIENT = MongoClient(EXTERNAL_URI, server_api=ServerApi('1'))
+EXTERNAL_CLIENT = MongoClient(EXTERNAL_URI, server_api=ServerApi("1"))
 
 # Send a ping to confirm a successful connection
 try:
-    EXTERNAL_CLIENT.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to EXTERNAL DATA RESOURCES DB")
+    EXTERNAL_CLIENT.admin.command("ping")
+    print(
+        "Pinged your deployment. You successfully connected to EXTERNAL DATA RESOURCES DB"
+    )
 except Exception as e:
     print(e)
 
-external_db = EXTERNAL_CLIENT.cirotex
 
-ETL_USER = os.getenv('DATALAKE_USER')
-ETL_PASSWORD = os.getenv('DATALAKE_PASSWORD')
-ETL_HOST = os.getenv('DATALAKE_HOST')
+ETL_USER = os.getenv("DATALAKE_USER")
+ETL_PASSWORD = os.getenv("DATALAKE_PASSWORD")
+ETL_HOST = os.getenv("DATALAKE_HOST")
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-ETL_URI = f"mongodb+srv://{ETL_USER}:{ETL_PASSWORD}@{ETL_HOST}/?retryWrites=true&w=majority"
+ETL_URI = (
+    f"mongodb+srv://{ETL_USER}:{ETL_PASSWORD}@{ETL_HOST}/?retryWrites=true&w=majority"
+)
 
 # Create a new client and connect to the server
 ETL_CLIENT = MongoClient(ETL_URI)
 
 # Send a ping to confirm a successful connection
 try:
-    ETL_CLIENT.admin.command('ping')
+    ETL_CLIENT.admin.command("ping")
     print("Pinged your deployment. You successfully connected to ETL DB")
 except Exception as e:
     print(e)
 
+external_db = EXTERNAL_CLIENT.cirotex
 inhouse_datalake_db = ETL_CLIENT.inHouseDataLake
 datawarehouse_db = ETL_CLIENT.warehouse
