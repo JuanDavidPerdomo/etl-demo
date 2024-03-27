aa9f8b4341eabd5fdd9fadacad7f455323147a77
Juan David Perdomo Quiroz <juanperdomo@JUAN>
2024-03-27 14:01:14 -0500

    removed if statement for validation on data_transformation file

--- data_transformation.py
+++ data_transformation.py
@@ -45,100 +45,96 @@ def data_cleaning_process():
         orders_df = pd.DataFrame(datalake_orders_raw_data)
 
         lenght_validation = len(orders_df.values.tolist())
+        filtered_orders = orders_df.copy()
+        filtered_orders = filtered_orders.loc[
+            (
+                (filtered_orders["status"] == "closed")
+                & (filtered_orders["unidades_vendidas"] > 0)
+            )
+        ]
 
-        if (lenght_validation == 0):
-            print(f'no tengo información para insertar. El df transformado a lista es de longitud: {lenght_validation}')
-        else:
-            filtered_orders = orders_df.copy()
-            filtered_orders = filtered_orders.loc[
-                (
-                    (filtered_orders["status"] == "closed")
-                    & (filtered_orders["unidades_vendidas"] > 0)
+        for col_name in filtered_orders.columns:
+            if type(filtered_orders[col_name][0]) == dict:
+                filtered_orders[col_name] = filtered_orders[col_name].apply(
+                    lambda x: (
+                        x["$oid"] if isinstance(x, dict) and "$oid" in x else x
+                    )
                 )
+            print(f"se limpió la columna: {col_name}\n")
+
+            # cleaning the reference dataframe to standatize to {'_id' : 'name'} format
+            references_df = references_df.drop(["categoryLevel1"], axis=1)
+
+            # needs refactory to convert into a function or a class that can allow to addapt more columns
+            references_df = references_df.rename(
+                columns={"_id": "referencia", "name": "nombre_referencia"}
+            )
+            categories_df = categories_df.rename(
+                columns={"_id": "categoria", "name": "nombre_categoria"}
+            )
+            shops_df = shops_df.rename(
+                columns={"_id": "tienda", "name": "nombre_tienda"}
+            )
+
+            index_dict = filtered_orders.index[
+                filtered_orders["categoria"].apply(lambda x: isinstance(x, dict))
             ]
+            filtered_orders = filtered_orders.drop(index_dict)
 
-            for col_name in filtered_orders.columns:
-                if type(filtered_orders[col_name][0]) == dict:
-                    filtered_orders[col_name] = filtered_orders[col_name].apply(
-                        lambda x: (
-                            x["$oid"] if isinstance(x, dict) and "$oid" in x else x
-                        )
-                    )
-                print(f"se limpió la columna: {col_name}\n")
+            filtered_orders["precio"] = filtered_orders["precio"].astype(int)
+            filtered_orders["unidades_vendidas"] = filtered_orders[
+                "unidades_vendidas"
+            ].astype(int)
+            filtered_orders["unidades_devueltas"] = filtered_orders[
+                "unidades_devueltas"
+            ].astype(int)
 
-                # cleaning the reference dataframe to standatize to {'_id' : 'name'} format
-                references_df = references_df.drop(["categoryLevel1"], axis=1)
+            size_mapper = {
+                "34": "XS",
+                "XL": "XL",
+                "Surtida": "Surtida",
+                "L": "L",
+                "M": "M",
+                "36": "L",
+                "32": "S",
+                "Surtido": "Surtido",
+                "Unica": "Unica",
+                "XXL": "XXL",
+                "38": "XL",
+                "S": "S",
+                "40": "XXL",
+                "30": "XS",
+                "L/XL": "L/XL",
+                "S/M": "S/M",
+            }
+            filtered_orders["talla"] = filtered_orders["talla"].map(size_mapper)
 
-                # needs refactory to convert into a function or a class that can allow to addapt more columns
-                references_df = references_df.rename(
-                    columns={"_id": "referencia", "name": "nombre_referencia"}
+            filtered_orders = filtered_orders.merge(
+                right=references_df, on="referencia", how="left"
                 )
-                categories_df = categories_df.rename(
-                    columns={"_id": "categoria", "name": "nombre_categoria"}
-                )
-                shops_df = shops_df.rename(
-                    columns={"_id": "tienda", "name": "nombre_tienda"}
-                )
+            filtered_orders = filtered_orders.merge(
+                right=categories_df, on="categoria", how="left"
+            )
+            filtered_orders = filtered_orders.merge(
+                right=shops_df, on="tienda", how="left"
+            )
 
-                index_dict = filtered_orders.index[
-                    filtered_orders["categoria"].apply(lambda x: isinstance(x, dict))
-                ]
-                filtered_orders = filtered_orders.drop(index_dict)
+            filtered_orders = tz_transform(
+                dataframe=filtered_orders, datetime_collumn_name="fecha_col"
+            )
 
-                filtered_orders["precio"] = filtered_orders["precio"].astype(int)
-                filtered_orders["unidades_vendidas"] = filtered_orders[
-                    "unidades_vendidas"
-                ].astype(int)
-                filtered_orders["unidades_devueltas"] = filtered_orders[
-                    "unidades_devueltas"
-                ].astype(int)
+            cleaned_df = filtered_orders.drop(
+                ["tienda", "categoria", "referencia", "_id"], axis=1
+            )
+            cleaned_df = cleaned_df.loc[~cleaned_df["nombre_referencia"].isna()]
 
-                size_mapper = {
-                    "34": "XS",
-                    "XL": "XL",
-                    "Surtida": "Surtida",
-                    "L": "L",
-                    "M": "M",
-                    "36": "L",
-                    "32": "S",
-                    "Surtido": "Surtido",
-                    "Unica": "Unica",
-                    "XXL": "XXL",
-                    "38": "XL",
-                    "S": "S",
-                    "40": "XXL",
-                    "30": "XS",
-                    "L/XL": "L/XL",
-                    "S/M": "S/M",
-                }
-                filtered_orders["talla"] = filtered_orders["talla"].map(size_mapper)
+            df_json = cleaned_df.to_dict(orient="records")
 
-                filtered_orders = filtered_orders.merge(
-                    right=references_df, on="referencia", how="left"
-                    )
-                filtered_orders = filtered_orders.merge(
-                    right=categories_df, on="categoria", how="left"
-                )
-                filtered_orders = filtered_orders.merge(
-                    right=shops_df, on="tienda", how="left"
-                )
+            master_collection.insert_many(df_json)
 
-                filtered_orders = tz_transform(
-                    dataframe=filtered_orders, datetime_collumn_name="fecha_col"
-                )
-
-                cleaned_df = filtered_orders.drop(
-                    ["tienda", "categoria", "referencia", "_id"], axis=1
-                )
-                cleaned_df = cleaned_df.loc[~cleaned_df["nombre_referencia"].isna()]
-
-                df_json = cleaned_df.to_dict(orient="records")
-
-                master_collection.insert_many(df_json)
-
-                return print(
-                    f"{len(df_json)} documentos insertados a las {datetime.now(utc_timezone)}"
-                )
+            return print(
+                f"{len(df_json)} documentos insertados a las {datetime.now(utc_timezone)}"
+            )
 
 
 
